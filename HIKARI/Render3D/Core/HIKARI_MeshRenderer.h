b5b0fcda794694af6e754ba2b1db9a6a8a969fc6
hikari39764-cell <hikari39764@gmail.com>
2026-05-07 02:36:39 +0900

    第二阶段：按节点层级提交模型Primitive

--- HIKARI/Render3D/Core/HIKARI_MeshRenderer.h
+++ HIKARI/Render3D/Core/HIKARI_MeshRenderer.h
@@ -5,6 +5,7 @@
 #include <DirectXMath.h>
 #include "Render3D/HIKARI_Camera3D.h"
 #include "Render3D/HIKARI_ModelAsset.h"
+#include "Render3D/HIKARI_Math3D.h"
 #include "Render3D/HIKARI_SceneEnvironment.h"
 #include "Render3D/HIKARI_Transform3D.h"
 
@@ -12,6 +13,7 @@ namespace HIKARI::MESHRENDERER {
 
     void Reset();
     void SubmitStaticMesh(const ModelAsset& asset, const Transform3D& transform, const std::string& materialFxProfileId, uint32_t postGroupMask, const DirectX::XMFLOAT4 (&materialFxParamValues)[4], bool materialFxValuesInitialized);
+    void SubmitStaticPrimitive(const ModelAsset& asset, uint32_t meshIndex, uint32_t primitiveIndex, const MATH::Mat4& worldMatrix, const MATH::Mat4& normalMatrix, const std::string& materialFxProfileId, uint32_t postGroupMask, const DirectX::XMFLOAT4 (&materialFxParamValues)[4], bool materialFxValuesInitialized);
     void RenderAll(const Camera3D& camera, const SceneEnvironment& environment);
 
 } // namespace HIKARI::MESHRENDERER
