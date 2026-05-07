3e6de33f4502adf72a5ed803ed145253ca310c2c
hikari39764-cell <hikari39764@gmail.com>
2026-05-07 15:29:05 +0900

    阶段二点五：显式矩阵参与Transform3D计算

--- HIKARI/Render3D/HIKARI_Transform3D.cpp
+++ HIKARI/Render3D/HIKARI_Transform3D.cpp
@@ -3,10 +3,16 @@
 namespace HIKARI {
 
     MATH::Mat4 Transform3D::GetLocalMatrix() const {
+        if (useExplicitMatrix) {
+            return explicitMatrix;
+        }
         return MATH::Mat4::TRS(position, MATH::NormalizeQ(rotation), scale);
     }
 
     MATH::Mat4 Transform3D::GetWorldMatrix() const {
+        if (useExplicitMatrix) {
+            return explicitMatrix;
+        }
         if (!parent) {
             return GetLocalMatrix();
         }
