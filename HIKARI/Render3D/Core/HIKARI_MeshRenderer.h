8b84698bf9c1431b4efd0ff1a6325d2c2cd927ed
hikari39764-cell <hikari39764@gmail.com>
2026-05-07 14:26:43 +0900

    整理未实现的MeshRenderer接口声明

--- HIKARI/Render3D/Core/HIKARI_MeshRenderer.h
+++ HIKARI/Render3D/Core/HIKARI_MeshRenderer.h
@@ -13,7 +13,6 @@ namespace HIKARI::MESHRENDERER {
 
     void Reset();
     void SubmitStaticMesh(const ModelAsset& asset, const Transform3D& transform, const std::string& materialFxProfileId, uint32_t postGroupMask, const DirectX::XMFLOAT4 (&materialFxParamValues)[4], bool materialFxValuesInitialized);
-    void SubmitStaticPrimitive(const ModelAsset& asset, uint32_t meshIndex, uint32_t primitiveIndex, const MATH::Mat4& worldMatrix, const MATH::Mat4& normalMatrix, const std::string& materialFxProfileId, uint32_t postGroupMask, const DirectX::XMFLOAT4 (&materialFxParamValues)[4], bool materialFxValuesInitialized);
     void RenderAll(const Camera3D& camera, const SceneEnvironment& environment);
 
 } // namespace HIKARI::MESHRENDERER
